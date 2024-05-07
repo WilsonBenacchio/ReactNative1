7b9d762eefe095068c789fdf3cf3d83247255fdc
WilsonBenacchio <simogato03@gmail.com>
2024-05-06 22:17:11 -0300

    update

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View, Image} from 'react-native';
-import { styles } from './src/class/style-externo';
+// import { Platform, StyleSheet } from "react-native";
+import { Platform } from "react-native";
+import { PageContainer, PageSubTittle, PageTittle, PageImage } from "./src/class/styled-externo";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.rimuru} source={require("./rimurureact.jpg")}></Image>
-      <Text style={styles.texto}>O melhor slime dos animes: Rimuru Tempest</Text>
-    </View>
+      <PageImage resizeMode="cover" source={Platform.OS === "android" ? require("./evelynnbk.jpg") : require("./rimurureact.jpg")}>
 
-  )}
+<PageTittle>O que vislumbras ao me contemplar?</PageTittle>
+        <PageSubTittle>Uma bruxa? Uma deusa? Ou algo mais...</PageSubTittle>
+      </PageImage>
+    
+  );
+}
+
+// const styles = StyleSheet.create({
+// container: {
+//   Flex: 1,
+//   backgroundColor: Platform.OS === 'android' ? 'black' : 'white'
+// }
+// })
---
+++ src/class/styled-externo.js
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+
+export const PageContainer = styled.View`
+display: flex;
+flex: 1;
+justify-content: center;
+align-items: center;
+background-color: black;
+
+` 
+
+
+export const PageTittle = styled.Text`
+font-size: 20px;
+color: white; 
+`
+
+export const PageSubTittle = styled.Text`
+font-size: 15px;
+color: white;
+`
+
+
+export const PageImage = styled.ImageBackground`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+flex: 1;
+`
+
+
